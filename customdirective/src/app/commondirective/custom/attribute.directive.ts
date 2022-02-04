0db80fa73cbe1ae5e389cbaa340d485351b00f01
gopika <gopikalakshmia@gmail.com>
2022-02-03 17:10:32 -0700

    Adding render property and hostlistner

--- customdirective/src/app/commondirective/custom/attribute.directive.ts
+++ customdirective/src/app/commondirective/custom/attribute.directive.ts
@@ -1,12 +1,24 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appAttribute]'
 })
-export class AttributeDirective {
 
-  constructor(private el:ElementRef) {
-    el.nativeElement.style.color="green";
+export class AttributeDirective {
+@Input() case:string="";
+  @HostListener('blur')onblur(){
+    let valuechanged:string=this.el.nativeElement.value.toUpperCase();
+    (this.case=="lower") ?this.render.setProperty(this.el.nativeElement,'value',valuechanged):
+    this.render.setProperty(this.el.nativeElement,'value',this.el.nativeElement.value.toLowerCase());
+  }
+
+  constructor(private el:ElementRef,private render:Renderer2) {
+    console.log("something");
+   }
+
+   ngOnInit(){
+
+    console.log(this.el.nativeElement.value);
    }
 
 }
